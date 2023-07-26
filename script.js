363b689d480ca9a44d9b0a5ca83b4d554c20508f
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-26 21:42:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ function strip(tourist){
 	return tourist.replace(/^(a |an |the )/i, '').trim();
 }
 const sorttourist = touristSpots.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+const li = sorttourist.forEach(item => {
+	`<li>${item}</li>`
+});
+document.getElemenById('band').innerHTML = li;
