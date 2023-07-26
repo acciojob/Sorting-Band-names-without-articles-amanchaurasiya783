5fe1cd696d913ecb35b060c34a9a7d0391e03fd4
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-26 21:14:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function strip(tourist){
+	return tourist.replace(/^(a |an |the )/i, '').trim();
+}
+const sorttourist = touristSpots.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+console.log(sorttourist);
