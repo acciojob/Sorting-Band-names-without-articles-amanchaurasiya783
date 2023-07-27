45072ccc634a52d59005fc8289a69f62d47e2f2a
amanchaurasiya783 <127719170+amanchaurasiya783@users.noreply.github.com>
2023-07-27 12:36:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 function strip(tourist){
-	return tourist.replace(/^(a |an |the )/i, '').trim();
+	return tourist.replace(/^(a |an |the )/i,'').trim();
 }
 const sorttourist = touristSpots.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
 document.getElementById("band").innerHTML = sorttourist.map(band => `<li>${band}</li>`)
